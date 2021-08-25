542dc4aa23222f84055b1e83b87b890dd01576ad
alvarobianor <bianor.alvaro12@gmail.com>
2021-08-25 17:43:47 -0300

    Using query to verify duplicated users

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
-import {query as q} from 'faunadb'
-import {fauna} from '../../../services/faunadb'
+import { query as q } from 'faunadb'
+import { fauna } from '../../../services/faunadb'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -12,23 +12,44 @@ export default NextAuth({
       scope: 'read:user'
     }),
     // ...add more providers here
-    
+
   ],
   // jwt: {
   //   signingKey: process.env.JWT_SIGNIN_KEY,
   // },
   callbacks: {
     async signIn(user, account, profile) {
-      const {email} = user
+      const { email } = user
       try {
         await fauna.query(
-          q.Create(q.Collection('users'), {
-            data: {
-              email
-            }
-          })
-        )     
-  
+          // q.Create(q.Collection('users'), {
+          //   data: {
+          //     email
+          //   }
+          // })
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(q.Collection('users'), {
+              data: {
+                email
+              }
+            }),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              )
+            )
+          )
+        )
+
         return true
       } catch {
         console.log('Erro')
