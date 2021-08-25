2ec6ce9e7154eecd2e2c57b47187ec0a0a3acf86
alvarobianor <bianor.alvaro12@gmail.com>
2021-08-25 17:47:09 -0300

    Remove warn to stripe file

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,10 +1,10 @@
 import Stripe from 'stripe'
 
-import {version} from '../../package.json'
+import Pack from '../../package.json'
 export const stripe = new Stripe(process.env.STRIBE_API_KEY, {
   apiVersion: "2020-08-27",
   appInfo: {
     name: 'Ignews',
-    version
+    version: Pack.version
   }
 })
