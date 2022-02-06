29ecb5ff28a4810700d2638badae11e1b8f4670c
alvaro bianor <bianor.alvaro12@gmail.com>
2022-02-06 17:28:19 -0300

    Events on webhooks on stripe

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,7 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function webhooks(req: NextApiRequest, res: NextApiResponse) {
-
-  console.log('Evento recebido')
-  return res.status(200).json({ ok: true })
+  console.log("Evento recebido", req.body);
+  return res.status(200).json({ ok: true });
 }
