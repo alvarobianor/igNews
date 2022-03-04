f29f825d7fdc13a87c8e3ea8e09e4d3ee1a79d00
Álvaro Medeiros <alvaro.medeiros@hp.com>
2022-03-03 21:23:29 -0300

    Navigation on menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,21 @@
-import { SignInGitHub } from '../SignInGitHub';
-import styles from './styles.module.scss';
-
+import { SignInGitHub } from "../SignInGitHub";
+import styles from "./styles.module.scss";
+import Link from "next/link";
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ignews" />
         <nav>
-
-          <a className={styles.active}>Home</a>
-          <a>Post</a>
+          <Link href="/">
+            <a className={styles.active}>Home</a>
+          </Link>
+          <Link href="/posts" prefetch>
+            <a>Post</a>
+          </Link>
         </nav>
         <SignInGitHub />
       </div>
     </header>
-  )
+  );
 }
